106a961f330eac1fa5c5e77a402ecec9b4621d0a
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-11-18 19:41:10 +0300

    on process

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+     handleChange = () => {
+        console.log('Entering password...');
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={this.handleChange}
+            /></div>
     )
 }
-
+}
 export default Keypad;
